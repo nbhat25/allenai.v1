224d45483451a283b6a0af8045f184d8b59e22c1
Nagaraj <nagaraj.bhat@ipsoft.com>
2015-10-08 19:21:20 +0530

    Initial commit

---
+++ allenai/src/main/java/net/airocks/allenai/App.java
@@ -0,0 +1,13 @@
+package net.airocks.allenai;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
